bd79a441411fc075049417b7f809b550beaa1569
Paulo-Heber <pauloheber2017@gmail.com>
2026-02-19 06:11:10 -0300

    refactor(layout): melhorar estrutura de layout e scroll da página
    Ajustado o container principal para crescer conforme o conteúdo.
    Corrigido problema do fundo que não permanecia branco ao rolar a página.
    Refinada a barra de navegação inferior flutuante.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
-import { FaCoins } from "react-icons/fa";
-import { GiHamburgerMenu } from "react-icons/gi";
+// ---------------IMPORT-COMPONENTS----------------
 import { CaptalCads } from './components/CaptalCards/captalCards';
+
+// ---------------IMPORT-ICONS----------------
+import { FaCoins, FaTools } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineShoppingBag } from "react-icons/md";
+import { PiRanking, PiTarget, PiHexagonBold } from "react-icons/pi";
 
 function App() {
   return (
@@ -10,8 +15,8 @@ function App() {
 
         {/* ------------HEADER---------------- */}
         <div className="menu-header">
-          <FaCoins className="menu-icon" />
-          <GiHamburgerMenu className="menu-hamburger" />
+          <FaCoins className="icon" />
+          <GiHamburgerMenu className="icon" />
         </div>
 
         {/* --------BODY---------------- */}
@@ -22,11 +27,11 @@ function App() {
           {/* ---------TAB BAR---------------- */}
 
           <ul className="tab-bar-container">
-            <li className="tab-bar-item">Capitais</li >
-            <li className="tab-bar-item">Ferramentas</li >
-            <li className="tab-bar-item">Loja</li >
-            <li className="tab-bar-item">Rankin</li >
-            <li className="tab-bar-item">Missões</li >
+            <li className="tab-bar-item"><PiHexagonBold /></li >
+            <li className="tab-bar-item"><FaTools /></li >
+            <li className="tab-bar-item"><MdOutlineShoppingBag /></li >
+            <li className="tab-bar-item"><PiRanking /></li >
+            <li className="tab-bar-item"><PiTarget /></li >
           </ul>
 
         </div>
--- src/components/CaptalCards/captalCards.jsx
+++ src/components/CaptalCards/captalCards.jsx
@@ -1,27 +1,31 @@
 import './captalCard.css';
 // --------------ICONES------------------ 
-import { FaWallet, FaHeart, FaDumbbell } from "react-icons/fa";
-import { GiBrain } from "react-icons/gi";
-import { MdSelfImprovement } from "react-icons/md";
-import { BsFire } from "react-icons/bs";
+import { LiaWalletSolid } from "react-icons/lia";
+import { LiaDumbbellSolid } from "react-icons/lia";
+import { LiaBookOpenSolid } from "react-icons/lia";
+import { PiFlowerLotusLight } from "react-icons/pi";
+import { LiaFireAltSolid } from "react-icons/lia";
+import { LiaHeart } from "react-icons/lia";
 
 export function CaptalCads() {
+
     const capitals = [
-        { name: "Físico", progress: 50 },
-        { name: "Financeiro", progress: 70 },
-        { name: "Intelectual", progress: 40 },
-        { name: "Espiritual", progress: 60 },
-        { name: "Emocional", progress: 80 },
-        { name: "Erótico", progress: 30 },
+        { name: "Físico", progress: 50, color: "#9EE5C7" },
+        { name: "Financeiro", progress: 70, color: "#9CCFDF" },
+        { name: "Intelectual", progress: 40, color: "#FCE9B1" },
+        { name: "Espiritual", progress: 60, color: "#C7C1ED" },
+        { name: "Emocional", progress: 80, color: "#F8BF92" },
+        { name: "Erótico", progress: 30, color: "#F98B7C" },
     ];
 
+
     const icons = {
-        Físico: FaDumbbell,
-        Financeiro: FaWallet,
-        Intelectual: GiBrain,
-        Espiritual: MdSelfImprovement,
-        Emocional: FaHeart,
-        Erótico: BsFire,
+        Físico: LiaDumbbellSolid,
+        Financeiro: LiaWalletSolid,
+        Intelectual: LiaBookOpenSolid,
+        Espiritual: PiFlowerLotusLight,
+        Emocional: LiaHeart,
+        Erótico: LiaFireAltSolid,
     };
 
 
@@ -30,20 +34,28 @@ export function CaptalCads() {
             {capitals.map((capital) => {
                 const IconComponent = icons[capital.name];
                 return (
-                    <li className='captal-card' key={capital.name} >
+                    <li className="captal-card"
+                        key={capital.name}
+                        style={{ "--card-color": capital.color }} >
 
                         <div className="captal-icon-container">
                             <IconComponent className="captal-icon" />
                         </div>
 
-                        <div>
-                            <p>{capital.name}</p>
-                            <div>
-                                <progress value={capital.progress} max="100" className="level-progress"></progress>
-                                <label >{capital.progress}%</label>
+                        <div className='captal-info'>
+                            <p >{capital.name}</p>
+                            <div className='progress-bar-status'>
+                                <div className="progress-bar">
+                                    <div
+                                        className="progress-fill"
+                                        style={{ width: `${capital.progress}%` }}
+                                    />
+                                </div>
+                                <label>{capital.progress}%</label>
+
                             </div>
                         </div>
-                        
+
                     </li>
                 );
             })}
